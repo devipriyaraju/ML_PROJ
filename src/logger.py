e5c34925a9ac06a22ad52e774e6f6987839d1420
devipriyaraju <devipriya.k.raju@gmail.com>
2026-04-04 20:41:43 -0600

    EDA,MODEL

--- src/logger.py
+++ src/logger.py
@@ -13,6 +13,6 @@ logging.basicConfig(
     format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO,
 )
-'''
+
 if __name__ == "__main__":
-    logging.info("Logging has been configured successfully.")'''
+    logging.info("Logging has been configured successfully.")
